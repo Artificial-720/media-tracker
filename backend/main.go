dbd888ba51e11f7192906e194ee2cbc84e30e90f
Artificial-720 <76878013+Artificial-720@users.noreply.github.com>
2025-08-21 14:37:18 -0500

    Simple server hosting static frontend files

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Starting Server")
+
+	router := mux.NewRouter()
+
+	// Frontend static files
+	staticPath := filepath.Join("..", "frontend")
+	fileServer := http.FileServer(http.Dir(staticPath))
+	router.PathPrefix("/").Handler(fileServer)
+
+	http.ListenAndServe(":8080", router)
 }
